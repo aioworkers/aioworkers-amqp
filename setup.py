8227ee1dc6a853328f4b18a584a359e385365745
Alexander Malev <yttrium@somedev.ru>
2017-07-01 02:38:35 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+requirements = [
+    'aioworkers',
+    'asynqp',
+]
+
+test_requirements = [
+    'pytest',
+]
+
+setup(
+    name='aioworkers-amqp',
+    version='0.1.0',
+    description="Module for working with amqp broker",
+    author="Alexander Malev",
+    author_email='yttrium@somedev.ru',
+    url='https://github.com/aioworkers/aioworkers-amqp',
+    packages=[i for i in find_packages() if i.startswith('aioworkers_amqp')],
+    install_requires=requirements,
+    license="Apache Software License 2.0",
+    keywords='aioworkers redis',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+)
